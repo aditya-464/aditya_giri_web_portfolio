f944ef3a8247a422130a1fb5b680a311d9e4acd6
Aditya <adityagiri1911@gmail.com>
2023-04-04 00:24:16 +0530

    About component completed

--- client/src/App.js
+++ client/src/App.js
@@ -10,10 +10,10 @@ import { Flex, Button } from '@chakra-ui/react';
 import { useState } from 'react';
 import { Practice2 } from './components/Practice2';
 import { lazy, Suspense } from 'react';
-import {MemoizedProjectDetailsRight} from './components/ProjectDetailsRight';
+import { MemoizedProjectDetailsRight } from './components/ProjectDetailsRight';
 import img1 from "./images/lp1.jfif";
 import img2 from "./images/lp2.jfif";
-import { About } from './components/About';
+import { MemoizedAbout } from './components/About';
 
 const MemoizedProject = lazy(() => import('./components/Project'));
 
@@ -23,14 +23,17 @@ function App() {
   return (
     <>
 
-      <About></About>
+      <Flex width="100%" height="100vh" flexDirection="row" backgroundColor="#FFE5B4">
+      </Flex>
+
+      <MemoizedAbout></MemoizedAbout>
       {/* <Navbar></Navbar> */}
       {/* <Hero></Hero> */}
-      
+
       {/* <MemoizedSectionName name="About Me"></MemoizedSectionName> */}
 
       {/* <MemoizedSectionName name="Projects"></MemoizedSectionName> */}
-      
+
       {/* <MemoizedSectionName name="Skills"></MemoizedSectionName> */}
       {/* <MemoizedSectionName name="Contact"></MemoizedSectionName> */}
 
@@ -41,8 +44,8 @@ function App() {
       {/* <Practice2></Practice2> */}
 
 
-      {/* <Flex width="100%" height="100vh" flexDirection="row" backgroundColor="#FFE5B4">
-      </Flex> */}
+      <Flex width="100%" height="100vh" flexDirection="row" backgroundColor="#FFE5B4">
+      </Flex>
 
       {/* <MemoizedProjectDetails number="01" title="Papyrus" techstack="React Js . Node Js . Express Js . MongoDB" description="Web application to post blogs and news
                             articles, along with relevant images"  img1={img1} img2={img2}></MemoizedProjectDetails> */}
--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,14 +1,42 @@
 import { Box, Text } from '@chakra-ui/react'
-import React from 'react'
+import React, { useRef, useState } from 'react'
 
 export const About = () => {
+
+  const [showItem, setShowItem] = useState(false);
+
+  const markRef1 = useRef(null);
+  const markRef2 = useRef(null);
+
+  const aboutFunc = (value)=>{
+    setShowItem(value);
+  }
+
+  window.addEventListener("scroll", () => {
+    const val1 = markRef1.current.getBoundingClientRect().top;
+    const val2 = markRef2.current.getBoundingClientRect().top;
+
+    if(val1<=400){
+      aboutFunc(true);
+    }
+    if(val1>=650 || val2<=100){
+      aboutFunc(false);
+    }
+  })
+
   return (
     <>
-        <Box className="about-container">
-            <Text className="about-content">
-                "  A web developer specializing in creating beautiful and functional websites for businesses. Utilizing programming expertise to deliver customized solutions that meet unique client needs. Dedicated to bringing visions to life! ❤️"
-            </Text>
-        </Box>
+      <Box className="about-container">
+        <Box ref={markRef1} className='markAbout1'></Box>
+        {showItem && <Box className='about-content-wrapper'>
+          <Text className="about-content">
+            "  A web developer specializing in creating beautiful and functional websites for businesses. Utilizing programming expertise to deliver customized solutions that meet unique client needs. Dedicated to bringing visions to life! ❤️"
+          </Text>
+        </Box>}
+        <Box ref={markRef2} className='markAbout2'></Box>
+      </Box>
     </>
   )
 }
+
+export const MemoizedAbout = React.memo(About);
