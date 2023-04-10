3fea5a6ce5a80962e34a6b60358f89a336e8174d
Aditya <adityagiri1911@gmail.com>
2023-04-10 08:19:55 +0530

    Mobile view of full project details page done

--- client/src/components/ProjectDetailsFull.jsx
+++ client/src/components/ProjectDetailsFull.jsx
@@ -16,13 +16,7 @@ export const ProjectDetailsFull = () => {
     const [showItem, setShowItem] = useState({
         image: false, text: false
     });
-
-    const imgContainerRef = useRef(null);
-    const imgRef1 = useRef(null);
-    const imgRef2 = useRef(null);
-    const imgRef3 = useRef(null);
-    const imgRef4 = useRef(null);
-
+   
     const mark1Ref = useRef(null);
     const mark2Ref = useRef(null);
 
@@ -40,24 +34,9 @@ export const ProjectDetailsFull = () => {
 
 
     window.addEventListener("scroll", () => {
-        // if (!imgContainerRef || !imgRef1 || !imgRef2 || !imgRef3 || !imgRef4) {
-        //     return;
-        // }
-
-        // const imgContainerHeight = imgContainerRef.current.getBoundingClientRect().top;
-
-
-        // let val = window.scrollY;
-
-        // imgRef1.current.style.top = val * -0.85 + "px";
-        // imgRef2.current.style.top = val * -0.95 + "px";
-        // imgRef3.current.style.top = val * -0.75 + "px";
-        // imgRef4.current.style.top = val * -0.65 + "px";
-
-        if (!mark1Ref || !mark2Ref) {
+        if (mark1Ref==null || mark2Ref==null) {
             return;
         }
-
         const val1 = mark1Ref.current.getBoundingClientRect().top;
         const val2 = mark2Ref.current.getBoundingClientRect().top;
 
@@ -67,9 +46,6 @@ export const ProjectDetailsFull = () => {
         if (val2 <= 0.25 * viewport.height) {
             setShowItem({ ...showItem, text: true });
         }
-
-
-
     })
 
     return (
@@ -82,16 +58,16 @@ export const ProjectDetailsFull = () => {
                 </Box>
                 <MemoizedSectionName name="Images"></MemoizedSectionName>
                 <Box className='proj-details-full-image-container'>
-                    <Box ref={mark1Ref} className='projFullMark1'></Box>
+                    <Box ref={mark1Ref} className='projFullMark1' border="2px solid red"></Box>
                     {showItem.image && <Box className='proj-details-full-image-content'>
-                        <img ref={imgRef1} src={img1} className='imgFull1' alt='img1'></img>
-                        <img ref={imgRef2} src={img2} className='imgFull2' alt='img2'></img>
-                        <img ref={imgRef3} src={img3} className='imgFull3' alt='img3'></img>
+                        <img src={img1} className='imgFull1' alt='img1'></img>
+                        <img src={img2} className='imgFull2' alt='img2'></img>
+                        <img src={img3} className='imgFull3' alt='img3'></img>
                     </Box>}
                 </Box>
                 <MemoizedSectionName name="Description"></MemoizedSectionName>
                 <Box className='project-details-full-description-container'>
-                    <Box ref={mark2Ref} className='projFullMark2'></Box>
+                    <Box ref={mark2Ref} className='projFullMark2' border="2px solid red"></Box>
                     {showItem.text && <Text className='project-details-full-description'>
                         <span>The text editing website created with React JS and CSS is a powerful and efficient platform for editing and formatting written content. The website is designed with a user-friendly interface and offers a wide range of tools and features for creating, editing, and publishing text-based content. React JS is a popular open-source JavaScript library that enables developers to create highly interactive and responsive user interfaces. The website created with React JS offers a smooth and seamless experience for users and provides real-time updates as changes are made to the text. CSS, on the other hand, is a style sheet language used for describing the presentation of a document written in HTML. The website created with CSS offers a visually appealing design, making it easier for users to navigate and use the platform. Overall, the text editing website created with React JS and CSS offers an intuitive and powerful platform for creating and editing text-based content, making it a top choice for writers, editors, and content creators.</span></Text>}
                 </Box>
