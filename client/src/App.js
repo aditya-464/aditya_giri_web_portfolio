4b16ee9b97f357fb44a6eaf903b9f2064d37d689
Aditya <adityagiri1911@gmail.com>
2023-04-16 07:24:44 +0530

    Preloaded hero section image

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css"
 import { Routes, Route } from "react-router-dom"
 import { lazy, Suspense } from "react";
+import heroimg from "./images/AGhero.svg"
 const MemoizedHomePage = lazy(() => import('./components/HomePage'));
 const MemoizedProjectDetailsFull = lazy(() => import('./components/ProjectDetailsFull'));
 function App() {
   return (
     <>
+    <img className="display-none" src={heroimg} alt="hero-img"></img>
       <Routes>
         <Route exact path='/' element={<Suspense><MemoizedHomePage></MemoizedHomePage></Suspense>}></Route>
         <Route exact path="/projDetail" element={<Suspense><MemoizedProjectDetailsFull></MemoizedProjectDetailsFull></Suspense>}></Route>
